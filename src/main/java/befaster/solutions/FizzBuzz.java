c2c6157b874a8e74947f42cc54d4f197c6f4f57f
sbx_user1059 <sbx_user1059@ip-10-12-149-21.eu-west-2.compute.internal>
2018-04-23 20:55:58 +0000

    Mon Apr 23 20:55:58 UTC 2018

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -5,28 +5,56 @@ import befaster.runner.SolutionNotImplementedException;
 public class FizzBuzz {
 
     public static String fizzBuzz(Integer number) {
-        if ((number % 3 == 0 || String.valueOf(number).contains("3")) && (number % 5 == 0 || String.valueOf(number).contains("5"))) {
-            if (check(number) == true) {
-                return "fizz buzz deluxe";
-            } else {
-                return "fizz buzz";
-            }
-        } else if (number % 3 == 0 || String.valueOf(number).contains("3")) {
-            if (check(number) == true && (number / 10 != 0)) {
-                return "fizz deluxe";
-            } else {
-                return "fizz";
-            }
-        } else if (number % 5 == 0 || String.valueOf(number).contains("5")) {
-            if (check(number) == true && (number / 10 != 0)) {
-                return "buzz deluxe";
-            } else {
-                return "buzz";
-            }
-        } else if (check(number) == true && (number / 10 != 0)) {
-            return "deluxe";
-        } else {
-            return String.valueOf(number);
+        boolean oed = (number % 2 != 0) ? true : false;
+        switch (odd) {
+            case true:
+                if ((number % 3 == 0 || String.valueOf(number).contains("3")) && (number % 5 == 0 || String.valueOf(number).contains("5"))) {
+                    if (check(number) == true) {
+                        return "fizz buzz fake deluxe";
+                    } else {
+                        return "fizz buzz";
+                    }
+                } else if (number % 3 == 0 || String.valueOf(number).contains("3")) {
+                    if (check(number) == true && (number / 10 != 0)) {
+                        return "fizz fake deluxe";
+                    } else {
+                        return "fizz";
+                    }
+                } else if (number % 5 == 0 || String.valueOf(number).contains("5")) {
+                    if (check(number) == true && (number / 10 != 0)) {
+                        return "buzz fake deluxe";
+                    } else {
+                        return "buzz";
+                    }
+                } else if (check(number) == true && (number / 10 != 0)) {
+                    return "fake deluxe";
+                } else {
+                    return String.valueOf(number);
+                }
+            case false:
+                if ((number % 3 == 0 || String.valueOf(number).contains("3")) && (number % 5 == 0 || String.valueOf(number).contains("5"))) {
+                    if (check(number) == true) {
+                        return "fizz buzz deluxe";
+                    } else {
+                        return "fizz buzz";
+                    }
+                } else if (number % 3 == 0 || String.valueOf(number).contains("3")) {
+                    if (check(number) == true && (number / 10 != 0)) {
+                        return "fizz deluxe";
+                    } else {
+                        return "fizz";
+                    }
+                } else if (number % 5 == 0 || String.valueOf(number).contains("5")) {
+                    if (check(number) == true && (number / 10 != 0)) {
+                        return "buzz deluxe";
+                    } else {
+                        return "buzz";
+                    }
+                } else if (check(number) == true && (number / 10 != 0)) {
+                    return "deluxe";
+                } else {
+                    return String.valueOf(number);
+                }
         }
 
     }
@@ -45,4 +73,5 @@ public class FizzBuzz {
         }
         return equal;
     }
+
 }
