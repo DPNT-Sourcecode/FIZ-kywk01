41dbd7063454d12c719c43c935e878fc0158d73c
sbx_user1059 <sbx_user1059@ip-10-12-149-21.eu-west-2.compute.internal>
2018-04-23 21:19:11 +0000

    Mon Apr 23 21:19:11 UTC 2018

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -8,68 +8,49 @@ public class FizzBuzz {
         int odd = (number % 2 != 0) ? 1 : 0;
         if (odd == 1) {
             if ((number % 3 == 0 || String.valueOf(number).contains("3")) && (number % 5 == 0 || String.valueOf(number).contains("5"))) {
-                if (check(number) == true) {
+                if ((number % 3 == 0 && String.valueOf(number).contains("3")) && (number % 5 == 0 && String.valueOf(number).contains("5"))) {
                     return "fizz buzz fake deluxe";
                 } else {
                     return "fizz buzz";
                 }
             } else if (number % 3 == 0 || String.valueOf(number).contains("3")) {
-                if (check(number) == true && (number / 10 != 0)) {
+                if (number % 3 == 0 && String.valueOf(number).contains("3")) {
                     return "fizz fake deluxe";
                 } else {
                     return "fizz";
                 }
             } else if (number % 5 == 0 || String.valueOf(number).contains("5")) {
-                if (check(number) == true && (number / 10 != 0)) {
+                if (number % 5 == 0 && String.valueOf(number).contains("5")) {
                     return "buzz fake deluxe";
                 } else {
                     return "buzz";
                 }
-            } else if (check(number) == true && (number / 10 != 0)) {
-                return "fake deluxe";
             } else {
                 return String.valueOf(number);
             }
         } else {
             if ((number % 3 == 0 || String.valueOf(number).contains("3")) && (number % 5 == 0 || String.valueOf(number).contains("5"))) {
-                if (check(number) == true) {
+                if ((number % 3 == 0 && String.valueOf(number).contains("3")) && (number % 5 == 0 && String.valueOf(number).contains("5"))) {
                     return "fizz buzz deluxe";
                 } else {
                     return "fizz buzz";
                 }
             } else if (number % 3 == 0 || String.valueOf(number).contains("3")) {
-                if (check(number) == true && (number / 10 != 0)) {
+                if (number % 3 == 0 && String.valueOf(number).contains("3")) {
                     return "fizz deluxe";
                 } else {
                     return "fizz";
                 }
             } else if (number % 5 == 0 || String.valueOf(number).contains("5")) {
-                if (check(number) == true && (number / 10 != 0)) {
+                if (number % 5 == 0 && String.valueOf(number).contains("5")) {
                     return "buzz deluxe";
                 } else {
                     return "buzz";
                 }
-            } else if (check(number) == true && (number / 10 != 0)) {
-                return "deluxe";
             } else {
                 return String.valueOf(number);
             }
         }
-    }
-
-    public static boolean check(int number) {
-        int c1, c2;
-        boolean equal = true;
-        c1 = number % 10;
-        while (number != 0) {
-            c2 = number % 10;
-            number = number / 10;
-            if (c1 != c2) {
-                equal = false;
-                break;
-            };
-        }
-        return equal;
     }
 
 }
