217cc9fa1865c332fc856f83f43bb09f210ea478
sbx_user1059 <sbx_user1059@ip-10-12-149-21.eu-west-2.compute.internal>
2018-04-23 19:27:11 +0000

    Mon Apr 23 19:27:11 UTC 2018

--- src/main/java/befaster/solutions/Hello.java
+++ src/main/java/befaster/solutions/Hello.java
@@ -4,6 +4,7 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class Hello {
     public static String hello(String friendName) {
-        throw new SolutionNotImplementedException();
+      
+        return "Hello" + friendName;
     }
 }
