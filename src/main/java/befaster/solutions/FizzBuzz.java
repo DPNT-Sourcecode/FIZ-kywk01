3bf2b281110db5df35a93fe0b15e390a2d7a3ee3
sbx_user1059 <sbx_user1059@ip-10-12-149-21.eu-west-2.compute.internal>
2018-04-23 19:54:04 +0000

    Mon Apr 23 19:54:04 UTC 2018

--- src/main/java/befaster/solutions/FizzBuzz.java
+++ src/main/java/befaster/solutions/FizzBuzz.java
@@ -5,12 +5,12 @@ import befaster.runner.SolutionNotImplementedException;
 public class FizzBuzz {
 
     public static String fizzBuzz(Integer number) {
-        if (number % 3 == 0&& String.valueOf(number).contains("3")&& number % 5 == 0&&String.valueOf(number).contains("5")) {
+        if ((number % 3 == 0|| String.valueOf(number).contains("3"))&&( number % 5 == 0||String.valueOf(number).contains("5"))) {
             return "fizz buzz";
         } else {
-            if (number % 3 == 0 && String.valueOf(number).contains("3")) {
+            if (number % 3 == 0 || String.valueOf(number).contains("3")) {
                 return "fizz";
-            } else if (number % 5 == 0 && String.valueOf(number).contains("5")) {
+            } else if (number % 5 == 0 || String.valueOf(number).contains("5")) {
                 return "buzz";
             } else {
                 return String.valueOf(number);
